362628d4f1ccb051f82ba919a39519ab3f17a10a
오영교 <oey157@nate.com>
2025-01-08 16:59:53 +0900

    s

--- index.js
+++ index.js
@@ -103,3 +103,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     });
+    var swiper = new Swiper(".mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+        },
+      });
